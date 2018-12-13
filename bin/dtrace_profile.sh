d9419a4aef2b75b10d954b7a19fe177fc9674524
Jeremy Wall <jeremy@marzhillstudios.com>
2018-12-12 19:25:56 -0600

    TOOL: Add profile script helper for dtrace.

---
+++ bin/dtrace_profile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# AUTHOR:  Jeremy Wall (jw), jeremy@marzhillstudios.com
+set -x
+
+file=$1
+
+cargo build --release
+sudo dtrace -c "target/release/ucg build ${file}" -o out.stacks -n 'profile-997 /execname == "ucg"/ { @[ustack(100)] = count(); }'
+stackcollapse.pl out.stacks > collapsed.stacks
+cat collapsed.stacks | flamegraph.pl --minwidth 2.5 > perf_graph.svg
+rm -f out.stacks collapsed.stacks
