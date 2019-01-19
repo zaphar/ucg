7959391b907a14b6420f436a0a49a6637986c6ec
Jeremy Wall <jeremy@marzhillstudios.com>
2019-01-19 13:30:06 -0600

    MAINT: Remove cpuprofiler.
    I wasn't using it.

--- src/benches/parse.rs
+++ src/benches/parse.rs
@@ -17,14 +17,11 @@
 #[macro_use]
 extern crate bencher;
 extern crate abortable_parser;
-extern crate cpuprofiler;
 extern crate ucglib;
 
 use bencher::Bencher;
 
 use ucglib::iter::OffsetStrIter;
-
-//use cpuprofiler::PROFILER;
 
 use ucglib::parse::*;
 
@@ -65,35 +62,17 @@ fn parse_complex_tuple(b: &mut Bencher) {
 }
 
 fn parse_simple_list(b: &mut Bencher) {
-    //PROFILER
-    //    .lock()
-    //    .unwrap()
-    //    .start("./my-parse_list.profile")
-    //    .unwrap();
     b.iter(|| do_parse("[1, 2, 3];"));
-    //PROFILER.lock().unwrap().stop().unwrap();
 }
 
 fn parse_complex_tuple_list(b: &mut Bencher) {
-    //PROFILER
-    //    .lock()
-    //    .unwrap()
-    //    .start("./my-parse_list.profile")
-    //    .unwrap();
     let input = "[{foo=1}, {bar=2}, {quux=4}];";
     b.iter(|| do_parse(input));
-    //PROFILER.lock().unwrap().stop().unwrap();
 }
 
 fn parse_complex_nested_list(b: &mut Bencher) {
-    //PROFILER
-    //    .lock()
-    //    .unwrap()
-    //    .start("./my-parse_list.profile")
-    //    .unwrap();
     let input = "[[1,2], [3,4], [4,5]];";
     b.iter(|| do_parse(input));
-    //PROFILER.lock().unwrap().stop().unwrap();
 }
 
 fn parse_selector_tuple_head(b: &mut Bencher) {
