97c25e076f41fb54ac1ce31caec76589c5918481
Jeremy Wall <jeremy@marzhillstudios.com>
2019-09-18 19:10:17 -0500

    Make this a little more general.

--- bin/dtrace_profile.sh
+++ bin/dtrace_profile.sh
@@ -2,10 +2,10 @@
 # AUTHOR:  Jeremy Wall (jw), jeremy@marzhillstudios.com
 set -x
 
-file=$1
+cmd=$1
 
 cargo build --release
-sudo dtrace -c "target/release/ucg build ${file}" -o out.stacks -n 'profile-997 /execname == "ucg"/ { @[ustack(100)] = count(); }'
+sudo dtrace -c "target/release/ucg ${cmd}" -o out.stacks -n 'profile-997 /execname == "ucg"/ { @[ustack(100)] = count(); }'
 stackcollapse.pl out.stacks > collapsed.stacks
 cat collapsed.stacks | flamegraph.pl --minwidth 2.5 > perf_graph.svg
 rm -f out.stacks collapsed.stacks
