f199e0f8717ff8de0f79252e67d1b2cdda377d7d
Jeremy Wall <jeremy@marzhillstudios.com>
2019-10-17 18:05:08 -0500

    DEV: Handle Windows File errors

--- src/build/opcode/error.rs
+++ src/build/opcode/error.rs
@@ -77,8 +77,8 @@ impl From<regex::Error> for Error {
 
 impl From<std::io::Error> for Error {
     fn from(e: std::io::Error) -> Self {
-        let msg = match e.kind() {
-            io::ErrorKind::NotFound => format!("OSError: Path not found: {}", e),
+        let msg = match dbg!(e.kind()) {
+            io::ErrorKind::NotFound | io::ErrorKind::Other => format!("OSError: Path not found: {}", e),
             _ => format!("{}", e),
         };
         Error {
