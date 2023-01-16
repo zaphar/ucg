e1600b0ebe1a1226ed8e1e80390d266ea2fd3331
Jeremy Wall <jeremy@marzhillstudios.com>
2023-01-16 14:53:08 -0500

    update base64 crate

--- src/convert/b64.rs
+++ src/convert/b64.rs
@@ -2,7 +2,8 @@ use std::error::Error;
 use std::rc::Rc;
 use std::result::Result;
 
-use base64::{encode, encode_config, URL_SAFE};
+use base64::Engine;
+use base64::{engine::general_purpose::STANDARD, engine::general_purpose::URL_SAFE};
 
 use crate::build::Val;
 use crate::convert::traits::Importer;
@@ -14,9 +15,9 @@ pub struct Base64Importer {
 impl Importer for Base64Importer {
     fn import(&self, bytes: &[u8]) -> Result<Rc<Val>, Box<dyn Error>> {
         return if self.url_safe {
-            Ok(Rc::new(Val::Str(encode(bytes))))
+            Ok(Rc::new(Val::Str(STANDARD.encode(bytes))))
         } else {
-            Ok(Rc::new(Val::Str(encode_config(bytes, URL_SAFE))))
+            Ok(Rc::new(Val::Str(URL_SAFE.encode(bytes))))
         };
     }
 }
